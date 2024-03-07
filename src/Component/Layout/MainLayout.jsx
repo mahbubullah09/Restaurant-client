7bbb6d904fd7ce3166404e6632aa0f8786dfd553
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-03-07 21:47:13 +0600

    react router

---
+++ src/Component/Layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../Home/Home';
+
+const MainLayout = () => {
+    return (
+        <div>
+<h2>layout</h2>
+            <Home/>
+            
+        </div>
+    );
+};
+
+export default MainLayout;
