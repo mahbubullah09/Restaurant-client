6f622269ab8931eca76bfaf5d756500ae7567137
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-03-08 20:14:58 +0600

    navbar

--- src/Component/Layout/MainLayout.jsx
+++ src/Component/Layout/MainLayout.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Home from '../Home/Home';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import Navbar from '../Navbar/Navbar';
 
 const MainLayout = () => {
     return (
         <div>
 
-            <h2>Navbar</h2>
+           <Navbar/>
             <Outlet />
             <Footer/>
             
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Navbar = () => {
+
+    const navOption = < >
+
+    <div className='flex flex-col lg:flex-row gap-2 font-bold text-lg'>
+    <ul cla>HOME</ul>
+        <ul> CONTACT US </ul>
+        <ul>DASHBOARD</ul>
+        <ul>OUR MENU</ul>
+        <ul>OUR SHOP</ul>
+        <button className=' text-lg font-bold '> SING OUT</button>
+
+    </div>
+
+    </>
+    return (
+        <div>
+            <div className="navbar bg-[#15151580]">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navOption}
+                        </ul>
+                    </div>
+
+                    <div className='flex flex-col  text-white'>
+                        <h2 className=' font-extrabold text-3xl'>BISTRO BOSS</h2>
+                        <h3 className='font-bold text-2xl'>Restaurant</h3>
+                    </div>
+                   
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navOption}
+                       
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
