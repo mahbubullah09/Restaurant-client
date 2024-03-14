75b86aaa7403e29df2fb81b02408abb7b41de214
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-03-14 23:09:02 +0600

    shared menu design

--- src/Component/Menu/Menu.jsx
+++ src/Component/Menu/Menu.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SharedCover from '../SharedComponent/SharedCover';
+import menuImg from '../../assets/menu/banner3.jpg'
 
 const Menu = () => {
     return (
         <div>
 
-            <SharedCover/>
+            <SharedCover bimg={menuImg}
+            title={'OUR MENU'}
+            subTitle={'Would you like to try a dish?'}/>
             
         </div>
     );
--- src/Component/SharedComponent/SharedCover.jsx
+++ src/Component/SharedComponent/SharedCover.jsx
@@ -1,15 +1,14 @@
 
 
-const SharedCover = () => {
+const SharedCover = ({bimg, title, subTitle}) => {
     return (
         <div>
-            <div className="hero min-h-[70vh]" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
+            <div className="hero min-h-[70vh]" style={{ backgroundImage: `url("${bimg}")` }}>
+                <div className=""></div>
+                <div className="hero-content text-center text-neutral-content bg-[#15151599] px-72 py-16">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+                        <p className="mb-5">{subTitle}</p>
                     </div>
                 </div>
             </div>
