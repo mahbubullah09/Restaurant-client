6c9480084e0ae3bf402ec28d59c0eaf5889b43b1
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-03-19 18:39:46 +0600

    custom hook

--- src/Component/Menu/Menu.jsx
+++ src/Component/Menu/Menu.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import SharedCover from '../SharedComponent/SharedCover';
 import menuImg from '../../assets/menu/banner3.jpg'
+import useMenu from '../../hooks/useMenu';
 
 
 const Menu = () => {
+
+    const [menu] = useMenu()
+
+    const offered = menu?.filter(item => item.category === "offered")
+    const pizza = menu?.filter(item => item.category === "pizza")
+    const dessert = menu?.filter(item => item.category === "dessert")
+    const soup = menu?.filter(item => item.category === "soup")
+    const salad = menu?.filter(item => item.category === "salad")
+
+   
+
     return (
         <div>
 
