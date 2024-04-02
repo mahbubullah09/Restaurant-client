05646f2e6cf2a08b9d0ec70d99ead6eeb0126997
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-04-03 04:42:51 +0600

    updated

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -14,7 +14,7 @@ const axiosSecure = axios.create({
      // request interceptor to add authorization header for every secure call to teh api
      axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token')
-        console.log('request stopped by interceptors', token)
+        // console.log('request stopped by interceptors', token)
         config.headers.authorization = `Bearer ${token}`;
         return config;
     }, function (error) {
